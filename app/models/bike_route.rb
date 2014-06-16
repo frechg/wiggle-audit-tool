184e6d90d4c798ec847c27cb01b92ad020f4a105
Galen Frechette <galenfrechette@gmail.com>
2014-06-15 18:44:32 -0700

    Add block and intersection count and empty route message

--- app/models/bike_route.rb
+++ app/models/bike_route.rb
@@ -1,3 +1,11 @@
 class BikeRoute < ActiveRecord::Base
   has_many :route_segments, dependent: :destroy
+
+  def block_count
+    RouteSegment.where(bike_route_id: self.id, type: "Block").count
+  end
+
+  def intersection_count
+    RouteSegment.where(bike_route_id: self.id, type: "Intersection").count
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 WiggleAuditTool::Application.routes.draw do
   root 'bike_routes#index'
 
-  # get '/list', to: 'pages#list', as: 'list'
-  # get '/audit', to: 'pages#audit', as: 'audit'
-
   resources :bike_routes, only: [:index] do
     resources :route_segments, only: [:index, :show, :update]
   end
-
-  # link_to("View segments", [@bike_route, :route_segments])
-  # bike_routes/1/route_segments
 end
