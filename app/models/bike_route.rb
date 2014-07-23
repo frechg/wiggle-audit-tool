774d8af759cab2a6da772978dcfd8eeef455bb0b
Galen Frechette <galenfrechette@gmail.com>
2014-07-23 13:19:46 -0700

    Display number of completed segements for a route

--- app/models/bike_route.rb
+++ app/models/bike_route.rb
@@ -2,10 +2,28 @@ class BikeRoute < ActiveRecord::Base
   has_many :route_segments, dependent: :destroy
 
   def block_count
-    RouteSegment.where(bike_route_id: self.id, type: "Block").count
+    blocks.count
   end
 
   def intersection_count
-    RouteSegment.where(bike_route_id: self.id, type: "Intersection").count
+    intersections.count
+  end
+
+  def completed_blocks?
+    blocks.select(&:complete?).length
+  end
+
+  def completed_intersections?
+    intersections.select(&:complete?).length
+  end
+
+  private
+
+  def blocks
+    RouteSegment.where(bike_route_id: self.id, type: "Block")
+  end
+
+  def intersections
+    RouteSegment.where(bike_route_id: self.id, type: "Intersection")
   end
 end
