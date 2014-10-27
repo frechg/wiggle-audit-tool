df45972c8a8f030ec519af76fdb7daecd2a9aae2
Galen Frechette <galenfrechette@gmail.com>
2014-10-26 22:46:04 -0700

    Impliment basic answer table

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,4 +3,10 @@ class QuestionsController < ApplicationController
     @block_questions = Question.where(route_segment_type: 'Block')
     @intersection_questions  = Question.where(route_segment_type: 'Intersection')
   end
+
+  def show
+    @question = Question.find(params[:id])
+    @answers = @question.answers.sort_by { |a| a.score }
+    @bike_routes = BikeRoute.all
+  end
 end
